c302c81fe6a96895e0d019f50cbf887c3359c879
Ömer Eraslan <54361555+oeraslan@users.noreply.github.com>
2022-01-03 19:57:19 +0100

    lab4

--- Lab-4-Guess-Word-Challenge/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ Lab-4-Guess-Word-Challenge/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -1,18 +1,3 @@
-/*
- * Copyright (C) 2019 Google Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
 package com.example.android.guesstheword.screens.game
 
@@ -24,26 +9,24 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 
-/**
- * ViewModel containing all the logic needed to run the game
- */
+
 class GameViewModel : ViewModel() {
 
-    // The current word
+
     private val _word = MutableLiveData<String>()
     val word: LiveData<String>
         get() = _word
 
-    // The current score
+
     private val _score = MutableLiveData<Int>()
     val score: LiveData<Int>
         get() = _score
 
 
 
-    // The list of words - the front of the list is the next word to guess
+
     private lateinit var wordList: MutableList<String>
-    // Intializing the game finish property
+
     private val _eventfinish = MutableLiveData<Boolean>()
     val eventfinish: LiveData<Boolean>
         get() = _eventfinish
@@ -60,9 +43,7 @@ class GameViewModel : ViewModel() {
                 word.get(randomPosition - 1).toUpperCase()
     }
 
-    /**
-     * Resets the list of words and randomizes the order
-     */
+
     private fun resetList() {
         wordList = mutableListOf(
             "queen",
@@ -112,18 +93,16 @@ class GameViewModel : ViewModel() {
         timer.start()
     }
 
-    /**
-     * Callback called when the ViewModel is destroyed
-     */
+
     override fun onCleared() {
         super.onCleared()
-//        Log.i("GameViewModel", "GameViewModel destroyed!")
+
         timer.cancel()
     }
 
 
 
-    /** Methods for updating the UI **/
+
     fun onSkip() {
         _score.value = (score.value)?.minus(1)
         nextWord()
@@ -143,26 +122,24 @@ class GameViewModel : ViewModel() {
     }
 
 
-    /**
-     * Moves to the next word in the list.
-     */
+
     private fun nextWord() {
         if (wordList.isEmpty()) {
             resetList()
         } else {
-            //Select and remove a _word from the list
+
             _word.value = wordList.removeAt(0)
         }
     }
     companion object {
 
-        // Time when the game is over
+
         private const val DONE = 0L
 
-        // Countdown time interval
+
         private const val ONE_SECOND = 1000L
 
-        // Total time for the game
+
         private const val COUNTDOWN_TIME = 60000L
 
     }
--- Lab-4-Guess-Word-Challenge/app/src/main/java/com/example/android/guesstheword/screens/score/ScoreFragment.kt
+++ Lab-4-Guess-Word-Challenge/app/src/main/java/com/example/android/guesstheword/screens/score/ScoreFragment.kt
@@ -1,18 +1,4 @@
-/*
- * Copyright (C) 2019 Google Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 
 package com.example.android.guesstheword.screens.score
 
@@ -28,9 +14,7 @@ import androidx.navigation.fragment.findNavController
 import com.example.android.guesstheword.R
 import com.example.android.guesstheword.databinding.ScoreFragmentBinding
 
-/**
- * Fragment where the final score is shown, after the game is over
- */
+
 class ScoreFragment : Fragment() {
 
     private lateinit var viewModel: ScoreViewModel
@@ -42,7 +26,7 @@ class ScoreFragment : Fragment() {
             savedInstanceState: Bundle?
     ): View? {
 
-        // Inflate view and obtain an instance of the binding class.
+
         val binding: ScoreFragmentBinding = DataBindingUtil.inflate(
                 inflater,
                 R.layout.score_fragment,
@@ -55,11 +39,10 @@ class ScoreFragment : Fragment() {
                 .get(ScoreViewModel::class.java)
         binding.scoreViewModel = viewModel
 
-        // Specify the fragment view as the lifecycle owner of the binding.
-        // This is used so that the binding can observe LiveData updates
+
         binding.lifecycleOwner = viewLifecycleOwner
 
-        // Navigates back to game when button is pressed
+
         viewModel.eventPlayAgain.observe(this, Observer { playAgain ->
             if (playAgain) {
                 findNavController().navigate(ScoreFragmentDirections.actionRestart())
