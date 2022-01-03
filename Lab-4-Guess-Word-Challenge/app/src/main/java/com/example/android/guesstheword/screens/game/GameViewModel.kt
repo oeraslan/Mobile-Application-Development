f6ad8a497ced8c5b05c07a59a5daec10370a34c1
Ömer Eraslan <54361555+oeraslan@users.noreply.github.com>
2022-01-03 19:39:35 +0100

    LAB-4

---
+++ Lab-4-Guess-Word-Challenge/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -0,0 +1,169 @@
+/*
+ * Copyright (C) 2019 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.guesstheword.screens.game
+
+import android.os.CountDownTimer
+import android.text.format.DateUtils
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
+import androidx.lifecycle.ViewModel
+
+/**
+ * ViewModel containing all the logic needed to run the game
+ */
+class GameViewModel : ViewModel() {
+
+    // The current word
+    private val _word = MutableLiveData<String>()
+    val word: LiveData<String>
+        get() = _word
+
+    // The current score
+    private val _score = MutableLiveData<Int>()
+    val score: LiveData<Int>
+        get() = _score
+
+
+
+    // The list of words - the front of the list is the next word to guess
+    private lateinit var wordList: MutableList<String>
+    // Intializing the game finish property
+    private val _eventfinish = MutableLiveData<Boolean>()
+    val eventfinish: LiveData<Boolean>
+        get() = _eventfinish
+    private val _currentTime=MutableLiveData<Long>()
+    val currentTime:LiveData<Long>
+        get() = _currentTime
+    private val timer:CountDownTimer
+    val currentTimeString = Transformations.map(currentTime) { time ->
+        DateUtils.formatElapsedTime(time)
+    }
+    val wordHint=Transformations.map(word){word->
+        val randomPosition=(1..word.length).random()
+        "Current word has "+word.length+" letters"+"\nThe letter at position " + randomPosition + " is " +
+                word.get(randomPosition - 1).toUpperCase()
+    }
+
+    /**
+     * Resets the list of words and randomizes the order
+     */
+    private fun resetList() {
+        wordList = mutableListOf(
+            "queen",
+            "hospital",
+            "basketball",
+            "cat",
+            "change",
+            "snail",
+            "soup",
+            "calendar",
+            "sad",
+            "desk",
+            "guitar",
+            "home",
+            "railway",
+            "zebra",
+            "jelly",
+            "car",
+            "crow",
+            "trade",
+            "bag",
+            "roll",
+            "bubble"
+        )
+        wordList.shuffle()
+    }
+
+    init {
+        _score.value = 0
+        _word.value = ""
+        Log.i("GameViewModel", "GameViewModel created!")
+        resetList()
+        nextWord()
+        timer = object : CountDownTimer(COUNTDOWN_TIME, ONE_SECOND) {
+
+            override fun onTick(millisUntilFinished: Long)
+            {
+                _currentTime.value = millisUntilFinished/ONE_SECOND
+            }
+
+            override fun onFinish() {
+                _currentTime.value = DONE
+                ongamefinish()
+            }
+        }
+
+        timer.start()
+    }
+
+    /**
+     * Callback called when the ViewModel is destroyed
+     */
+    override fun onCleared() {
+        super.onCleared()
+//        Log.i("GameViewModel", "GameViewModel destroyed!")
+        timer.cancel()
+    }
+
+
+
+    /** Methods for updating the UI **/
+    fun onSkip() {
+        _score.value = (score.value)?.minus(1)
+        nextWord()
+    }
+
+    fun onCorrect() {
+        _score.value = (score.value)?.plus(1)
+        nextWord()
+    }
+
+    /** game complete event method **/
+    fun ongamefinish() {
+        _eventfinish.value = true
+    }
+    fun onGameFinishComplete() {
+        _eventfinish.value = false
+    }
+
+
+    /**
+     * Moves to the next word in the list.
+     */
+    private fun nextWord() {
+        if (wordList.isEmpty()) {
+            resetList()
+        } else {
+            //Select and remove a _word from the list
+            _word.value = wordList.removeAt(0)
+        }
+    }
+    companion object {
+
+        // Time when the game is over
+        private const val DONE = 0L
+
+        // Countdown time interval
+        private const val ONE_SECOND = 1000L
+
+        // Total time for the game
+        private const val COUNTDOWN_TIME = 60000L
+
+    }
+}
